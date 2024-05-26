12271cc9dbee4bde38f4f03a3de80f2dc0d879a5
neetchea <medouni.sarra@gmail.com>
2024-05-26 16:22:09 +0100

    added session user variable to todo.php,added session start to main, only logged in user can access main,added link to todo.php in mainlogin.php now redirects to todo.php,altered tasks table to have a task_name column,added tasks from database to todo.php,created tasks now are saved in tasks table,added delete task logic remove_task.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="./styles/style.css"?v=1.0>
+</head>
+<body>
+    <div class='container'>
+        <h1>Welcome to Your Todo app</h1>
+        <p>Discover amazing features and organize your life!</p>
+        <a href="register.php" class="button">Register</a>
+        <a href="login.php" class="button">Login</a>
+    </div>
+</body>
+</html>
